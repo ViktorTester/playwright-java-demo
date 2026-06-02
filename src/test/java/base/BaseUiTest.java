452be3035316bc201176e5a40e21f41cf5b2076d
ViktorTester <viktor345@inbox.lv>
2026-06-02 19:17:39 +0300

    minor fix

--- src/test/java/base/BaseUiTest.java
+++ src/test/java/base/BaseUiTest.java
@@ -10,9 +10,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
-import static tests.tags.TestTags.SMOKE;
-import static tests.tags.TestTags.UI;
-
 public abstract class BaseUiTest {
 
     protected static Playwright playwright;
